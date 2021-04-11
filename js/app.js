5f41e66f8fe315061d3e939a00fdb734bd4370ca
javiermelendrez1 <81501034+javiermelendrez1@users.noreply.github.com>
2021-04-10 17:34:46 -0700

    added a heart
    - when the second animation comes on a heart like if you liked the previous message appears

---
+++ js/app.js
@@ -0,0 +1,10 @@
+console.log('connected')
+const heart = document.querySelector('#heart');
+let count = 0;
+document.addEventListener('aos:in', ({ detail }) => {
+    console.log(detail);
+    count++;
+    if(count > 1){
+        heart.style.display = 'block'
+    }
+  });
